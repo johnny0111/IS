65392364482b77d04492bf538050bbbd9b71234f
johnny0111 <jmfde.carvalho@campus.fct.unl.pt>
2021-04-05 17:32:51 +0100

    parte 3_ incompleto
    Com o wolf não funciona

--- LAB1/IS_TP1_Sockets/src/is_tp1_sockets/IS_TP1_ServerSocketCow.java
+++ LAB1/IS_TP1_Sockets/src/is_tp1_sockets/IS_TP1_ServerSocketCow.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package is_tp1_serversocket;
+package is_tp1_sockets;
 
 import Common.MessageManagement;
 import java.io.BufferedReader;
--- LAB1/IS_TP1_Sockets/src/is_tp1_sockets/IS_TP1_ServerSocketWolf.java
+++ LAB1/IS_TP1_Sockets/src/is_tp1_sockets/IS_TP1_ServerSocketWolf.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package is_tp1_serversocket;
+package is_tp1_sockets;
 
 import Common.MessageManagement;
 import java.io.BufferedReader;
@@ -91,5 +86,3 @@ public class IS_TP1_ServerSocketWolf {
 
 
 }
- 
-
